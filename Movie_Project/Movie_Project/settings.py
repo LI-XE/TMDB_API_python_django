3498db3771eb8ade8eeaed2fd2b53bc5892fb5ea
LI_XE <hyangeun312@gmail.com>
2023-08-21 08:35:54 -0500

    update

--- Movie_Project/Movie_Project/settings.py
+++ Movie_Project/Movie_Project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-e_ae4q4%v$^vl#ykytj504r_13!!w+1@mqf5u^zxbq5p(obc1b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
